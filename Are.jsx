de2e4cb1604a2ae35b698a4bdc793d8dcefb5787
Ahmed Ismail <ismail.uet@gmail.com>
2019-05-27 10:10:48 +0200

    Jsx files

---
+++ Are.jsx
@@ -0,0 +1,151 @@
+<div>
+  <title>Basic 88</title>
+  <meta charSet="UTF-8" />
+  <link rel="stylesheet" href="layout.css" type="text/css" />
+  {/*[if lt IE 9]><![endif]*/}
+  <div className="wrapper row1">
+    <header id="header" className="clear">
+      <div id="hgroup">
+        <h1><a href="#">Åre</a></h1>
+        <h2>Leading ski resort</h2>
+      </div>
+      <nav>
+        <ul>
+          <li><a href="#intro">About</a></li>
+          <li><a href="#services">Attractions</a></li>
+          <li><a href="#services">Transport</a></li>
+          <li><a href="#services">Accommodation</a></li>
+          {/*        <li class="last"><a href="#">Text Link</a></li> */}
+        </ul>
+      </nav>
+    </header>
+  </div>
+  {/* content */}
+  <div className="wrapper row2">
+    <div id="container" className="clear">
+      {/* Slider */}
+      <section id="slider"><a href="#"><img src="offpist-are-AA-Are.jpg" width={960} height={360} alt /></a></section>
+      {/* main content */}
+      <div id="homepage">
+        {/* Services */}
+        <section id="services" className="clear">
+          <article className="one_third">
+            <figure><img src="crop_media_1254_foto-ola-rockberg-kabin-1-are.jpg" width={290} height={180} alt />
+              <figcaption>
+                <h2>Places to See in Åre</h2>
+                <p>Åre's cable car takes you up to 1274 MASL, making Åreskutan the highest most accessible mountain in Sweden.</p>
+                <footer className="more"><a href="#footer">Read More »</a></footer>
+              </figcaption>
+            </figure>
+          </article>
+          <article className="one_third">
+            <figure><img src="60866c60ece0563e1a9103c5ef22edaa624cc6ef-are.jpg" width={290} height={180} alt />
+              <figcaption>
+                <h2>Transport</h2>
+                <p>There is a local transport system in the city.</p>
+                <footer className="more"><a href="#footer">Read More »</a></footer>
+              </figcaption>
+            </figure>
+          </article>
+          <article className="one_third lastbox">
+            <figure><img src="interior_room_lookin_out_copperhill_480-are.jpg" width={290} height={180} alt />
+              <figcaption>
+                <h2>Places to stay in Åre</h2>
+                <p>You can choose from many hotels to stay in Falun.</p>
+                <footer className="more"><a href="#footer">Read More »</a></footer>
+              </figcaption>
+            </figure>
+          </article>
+        </section>
+        {/* / Services */}
+        {/* ########################################################################################## */}
+        {/* ########################################################################################## */}
+        {/* ########################################################################################## */}
+        {/* ########################################################################################## */}
+        {/* Introduction */}
+        <section id="intro" className="last clear">
+          <article>
+            <figure><img src="Åre_Björnen_Stugby_@20100611_TOJ6225-are.jpg" width={450} height={250} alt />
+              <figcaption>
+                <h2>About the City of Åre</h2>
+                <p>Åre is a locality and one of the leading Scandinavian ski resorts situated in Åre Municipality, Jämtland County, Sweden with 1,417 inhabitants in 2010. 25% of the municipal industry is based on tourism, most notably the downhill skiing and biking resorts in Åre, Duved and Storlien. The growth in tourism has resulted in the development of hotels, recreational and shopping opportunities in the area.</p>
+                {/*              <p> template from <a href="https://www.os-templates.com/" title="Free Website Templates">OS Templates</a> read our <a href="https://www.os-templates.com/template-terms">website template licence</a>.</p>
+        <p> visit <a href="https://www.os-templates.com/">free website templates</a>.</p> */}
+                <footer className="more"><a href="https://en.wikipedia.org/wiki/%C3%85re">Read More »</a></footer>
+              </figcaption>
+            </figure>
+          </article>
+        </section>
+        {/* / Introduction */}
+      </div>
+      {/* / content body */}
+    </div>
+  </div>
+  {/* Footer */}
+  <div className="wrapper row3">
+    <div id="footer" className="clear">
+      {/* Section One */}
+      <section className="one_quarter">
+        <h2 className="title">About Åre</h2>
+        <nav>
+          <ul>
+            <li><a href="#">Welcome to Åre</a></li>
+            <li><a href="#">More about Åre</a></li>
+            <li><a href="#">Offical website of Åre</a></li>
+            <li><a href="#">History of Åre</a></li>
+            <li className="last"><a href="#">Weather in Åre</a></li>
+          </ul>
+        </nav>
+      </section>
+      {/* Section Two */}
+      <section className="one_quarter">
+        <h2 className="title">Attractions</h2>
+        <nav>
+          <ul>
+            <li><a href="#">Things to do in Åre</a></li>
+            <li><a href="#">Sightseeing</a></li>
+            <li><a href="#">Attractions in Åre</a></li>
+            <li><a href="#">Recommendations form tripadvisor</a></li>
+            <li className="last"><a href="#">Attractions</a></li>
+          </ul>
+        </nav>
+      </section>
+      {/* Section Three */}
+      <section className="one_quarter">
+        <h2 className="title">Transport</h2>
+        <nav>
+          <ul>
+            <li><a href="#">Local Transport</a></li>
+            <li><a href="#">Train service from Stockholm</a></li>
+            <li><a href="#">Fly from stockholm</a></li>
+            <li><a href="#">Buss service from stockholm</a></li>
+            <li className="last"><a href="#">Taxi service</a></li>
+          </ul>
+        </nav>
+      </section>
+      {/* Section Four */}
+      <section className="one_quarter lastbox">
+        <h2 className="title">Accommodation</h2>
+        <nav>
+          <ul>
+            <li><a href="#">Holiday club</a></li>
+            <li><a href="#">Hotell Granen</a></li>
+            <li><a href="#">Hotell Fjällgården</a></li>
+            <li><a href="#">Hotel Åregården</a></li>
+            <li className="last"><a href="#">Copperhill Mountain Lodge</a></li>
+          </ul>
+        </nav>
+      </section>
+      {/* / Section */}
+    </div>
+  </div>
+  {/* Copyright */}
+  <div className="wrapper row4">
+    <footer id="copyright" className="clear">
+      <p className="fl_left">Copyright © 2019 - All Rights Reserved - <a href="#">2Sweden</a></p>
+      <p className="fl_right">Contact Us {/* <a target="_blank" href="https://www.os-templates.com/" title="Free Website Templates">OS Templates</a> */} </p>
+      {/*    <p class="fl_left">Copyright &copy; 2018 - All Rights Reserved - <a href="#">Domain Name</a></p>
+    <p class="fl_right">Template by <a target="_blank" href="https://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p> */}
+    </footer>
+  </div>
+</div>
---
+++ sthlm.jsx
@@ -0,0 +1,149 @@
+<div>
+  <title>Basic 88</title>
+  <meta charSet="UTF-8" />
+  <link rel="stylesheet" href="layout.css" type="text/css" />
+  {/*[if lt IE 9]><![endif]*/}
+  <div className="wrapper row1">
+    <header id="header" className="clear">
+      <div id="hgroup">
+        <h1><a href="#">Stockholm</a></h1>
+        <h2>Venice of the north</h2>
+      </div>
+      <nav>
+        <ul>
+          <li><a href="#intro">About</a></li>
+          <li><a href="#services">Attractions</a></li>
+          <li><a href="#services">Transport</a></li>
+          <li><a href="#services">Accommodation</a></li>
+          {/*        <li class="last"><a href="#">Text Link</a></li> */}
+        </ul>
+      </nav>
+    </header>
+  </div>
+  {/* content */}
+  <div className="wrapper row2">
+    <div id="container" className="clear">
+      {/* Slider */}
+      <section id="slider"><a href="#"><img src="eur-sweden-stockholm.jpg" alt /></a></section>
+      {/* main content */}
+      <div id="homepage">
+        {/* Services */}
+        <section id="services" className="clear">
+          <article className="one_third">
+            <figure><img src="images.jpg" width={290} height={180} alt />
+              <figcaption>
+                <h2>Places to See in Stockholm</h2>
+                <p>Stockholm has a lot of Attractions.</p>
+                <footer className="more"><a href="#footer">Read More »</a></footer>
+              </figcaption>
+            </figure>
+          </article>
+          <article className="one_third">
+            <figure><img src="Forarlosatag.jpg" width={290} height={180} alt />
+              <figcaption>
+                <h2>Local Transport</h2>
+                <p>Stockholm has an big public transport system.</p>
+                <footer className="more"><a href="#footer">Read More »</a></footer>
+              </figcaption>
+            </figure>
+          </article>
+          <article className="one_third lastbox">
+            <figure><img src="f37c2f1c891f0dc3313fccfb9d3df96e.jpg" width={290} height={180} alt />
+              <figcaption>
+                <h2>Places to stay in Stockholm</h2>
+                <p>There is vide range of Hotels to stay in Stockholm.</p>
+                <footer className="more"><a href="#footer">Read More »</a></footer>
+              </figcaption>
+            </figure>
+          </article>
+        </section>
+        {/* / Services */}
+        {/* ########################################################################################## */}
+        {/* ########################################################################################## */}
+        {/* ########################################################################################## */}
+        {/* ########################################################################################## */}
+        {/* Introduction */}
+        <section id="intro" className="last clear">
+          <article>
+            <figure><img src="stockholm-1024x681.jpg" width={450} height={250} alt />
+              <figcaption>
+                <h2>About the City of Stockholm</h2>
+                <p>Stockholm is the capital of Sweden and the most populous urban area in the Nordic countries. The city stretches across fourteen islands where Lake Mälaren flows into the Baltic Sea. Outside the city to the east, and along the coast, is the island chain of the Stockholm archipelago. Stockholm is the cultural, media, political, and economic centre of Sweden. The Stockholm region alone accounts for over a third of the country's GDP. It is an important global city and the main centre for corporate headquarters in the Nordic region.</p>
+                {/*              <p> template from <a href="https://www.os-templates.com/" title="Free Website Templates">OS Templates</a> read our <a href="https://www.os-templates.com/template-terms">website template licence</a>.</p>
+        <p> visit <a href="https://www.os-templates.com/">free website templates</a>.</p> */}
+                <footer className="more"><a href="https://en.wikipedia.org/wiki/Stockholm">Read More »</a></footer>
+              </figcaption>
+            </figure>
+          </article>
+        </section>
+        {/* / Introduction */}
+      </div>
+      {/* / content body */}
+    </div>
+  </div>
+  {/* Footer */}
+  <div className="wrapper row3">
+    <div id="footer" className="clear">
+      {/* Section One */}
+      <section className="one_quarter">
+        <h2 className="title">About stockholm</h2>
+        <nav>
+          <ul>
+            <li><a href="https://en.wikipedia.org/wiki/Stockholm">More About Stockholm</a></li>
+            <li><a href="https://www.stockholm.se/">Offical website of Stockholm</a></li>
+            <li><a href="#">History of Stockholm</a></li>
+            <li><a href="#">Weather in Stockholm</a></li>
+            <li className="last"><a href="#">Night life in Stockholm</a></li>
+          </ul>
+        </nav>
+      </section>
+      {/* Section Two */}
+      <section className="one_quarter">
+        <h2 className="title">Attractions</h2>
+        <nav>
+          <ul>
+            <li><a href="https://www.visitstockholm.com/">Things to do in Stockholm</a></li>
+            <li><a href="https://www.stromma.com/sv-se/stockholm/sightseeing/?gclid=EAIaIQobChMItbnjq6Kx4gIVUy4YCh076g6CEAAYASAAEgKtGfD_BwE">Sightseeing</a></li>
+            <li><a href="https://www.tripadvisor.com/Attractions-g189852-Activities-Stockholm.html">recommendations from tripadvisor</a></li>
+            <li><a href="#">Attractions</a></li>
+            <li className="last"><a href="#">Attractions</a></li>
+          </ul>
+        </nav>
+      </section>
+      {/* Section Three */}
+      <section className="one_quarter">
+        <h2 className="title">Transport</h2>
+        <nav>
+          <ul>
+            <li><a href="https://sl.se/">Local Transport</a></li>
+            <li><a href="https://www.arlandaexpress.com/">Train service for Arlanda Airport</a></li>
+            <li><a href="https://www.flygbussarna.se/">Buss Services to Airports</a></li>
+            <li><a href="https://www.sj.se/#/">Train service to other Cities</a></li>
+            <li className="last"><a href="https://www.taxistockholm.se/en/">Taxi Services</a></li>
+          </ul>
+        </nav>
+      </section>
+      {/* Section Four */}
+      <section className="one_quarter lastbox">
+        <h2 className="title">Accommodation</h2>
+        <nav>
+          <ul>
+            <li><a href="https://www.sheratonstockholm.se/">sheraton stockholm</a></li>
+            <li><a href="https://www.scandichotels.com/hotels/sweden/stockholm">scandichotels Stockholm</a></li>
+            <li><a href="https://www.grandhotel.se/en">Grand hotel Stockholm</a></li>
+            <li><a href="https://www.hiltonhotels.com/sv_SE/sverige/hilton-stockholm-slussen/">hilton hotel Stockholm</a></li>
+            <li className="last"><a href="https://www.elite.se/en/hotels/stockholm/hotel-stockholm-plaza/">elite hotel Stockholm</a></li>
+          </ul>
+        </nav>
+      </section>
+      {/* / Section */}
+    </div>
+  </div>
+  {/* Copyright */}
+  <div className="wrapper row4">
+    <footer id="copyright" className="clear">
+      <p className="fl_left">Copyright © 2019 - All Rights Reserved - <a href="#">2Sweden</a></p>
+      <p className="fl_right">Contact Us {/* <a target="_blank" href="https://www.os-templates.com/" title="Free Website Templates">OS Templates</a> */} </p>
+    </footer>
+  </div>
+</div>
